2a3c0b4aa7ffd89b2b6c72831c3f5eb824c1412e
kantale <alexandros.kanterakis@gmail.com>
2015-09-26 00:40:16 +0300

    added prefilled.py

--- pypedia/prefilled.py
+++ pypedia/prefilled.py
@@ -1,4 +1,5 @@
-{{#form:action=<nowiki>http://www.pypedia.com/extensions/PyPedia_server/pypdownload.php</nowiki>|method=post|target=_blank|id=header_form|enctype=multipart/form-data}}{{#input:type=hidden|name=article_title|value=@PYPEDIAARTICLENAME@}}{{#input:type=hidden|name=pyp_username|value={{CURRENTUSER}}}}{{#input:type=ajax|value=Fork this article|id=fa}}{{#formend:}}
+
+prefilled_text = """{{#form:action=<nowiki>http://www.pypedia.com/extensions/PyPedia_server/pypdownload.php</nowiki>|method=post|target=_blank|id=header_form|enctype=multipart/form-data}}{{#input:type=hidden|name=article_title|value=@PYPEDIAARTICLENAME@}}{{#input:type=hidden|name=pyp_username|value={{CURRENTUSER}}}}{{#input:type=ajax|value=Fork this article|id=fa}}{{#formend:}}
 ==Documentation==
 
 Documentation for '''@PYPEDIAARTICLENAMENOUS@'''
@@ -69,4 +70,4 @@ def @PYPEDIAARTICLENAME@():
 ===Permissions Permissions===
 
 @PYPEDIAUSERNAME@
-
+"""
