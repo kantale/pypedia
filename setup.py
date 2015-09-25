9f558c0bdeff500b3b54011c4c45be2bd5d4e285
kantale <alexandros.kanterakis@gmail.com>
2015-09-26 00:44:14 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(name='pypedia',
+      version='1.0',
+      description='Library for local execution of pypedia methods',
+      url='https://github.com/kantale/pypedia',
+      author='Alexandros Kanterakis',
+      author_email='alexandros.kanterakis@gmail.com',
+      classifiers=[
+      ],
+      install_requires=[
+      ],
+      packages=['pypedia', 'pypmwclient'],
+)
